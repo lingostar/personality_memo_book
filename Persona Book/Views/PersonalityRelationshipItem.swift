9ff77ccf10a4c27bf0781a729fe621dce129e857
pagh2322 <pagh2322@naver.com>
2022-05-18 11:55:35 +0900

    Add Icon & Modal View for showing persona list in relationlistitem

--- Persona Book/Views/PersonalityRelationshipItem.swift
+++ Persona Book/Views/PersonalityRelationshipItem.swift
@@ -10,11 +10,37 @@ import SwiftUI
 struct PersonalityRelationshipItem: View {
     var realtionText: String
     var personalityList: [Personality]
+    @State var showPersona = false
+    @EnvironmentObject var viewModel: MainVM
     
     var body: some View {
         NavigationLink(destination:
             List(self.personalityList, id: \.self) { personality in
-                Text(personality.title)
+                HStack {
+                    Button("\(personality.title)") {
+                        self.showPersona.toggle()
+                    }
+                    .foregroundColor(.primary)
+                    Spacer()
+                    Text("\(self.viewModel.personas[personality.rawValue].count)")
+                        .foregroundColor(.secondary)
+                }
+                .sheet(isPresented: self.$showPersona) {
+                    NavigationView {
+                        List(self.viewModel.personas[personality.rawValue]) { persona in
+                            Text(persona.name)
+                        }
+                        .navigationBarTitle("PersonaList".localized())
+                        .navigationBarTitleDisplayMode(.inline)
+                        .toolbar {
+                            ToolbarItem(placement: .cancellationAction) {
+                                Button("Cancel".localized()) {
+                                    self.showPersona.toggle()
+                                }
+                            }
+                        }
+                    }
+                }
             }
             .navigationTitle(realtionText)
             .navigationBarTitleDisplayMode(.inline)
